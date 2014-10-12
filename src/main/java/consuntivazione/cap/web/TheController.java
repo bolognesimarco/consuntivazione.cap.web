9fbb95ffc9083539622b5fbcfb425cca47fe85ce
miogituser <bolognesi.marco@gmail.com>
2014-10-12 22:36:07 +0200

    no news

--- src/main/java/consuntivazione/cap/web/TheController.java
+++ src/main/java/consuntivazione/cap/web/TheController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,24 +14,27 @@ import consuntivazione.cap.vo.ReportEntryVO;
 
 @Controller
 public class TheController {
-	
+
 	@Autowired
 	private TimeSheetService timeSheetService;
 
-	@RequestMapping(value="/report")
-	@ResponseBody
-	public List<ReportEntryVO> getReportEntries(
-			@RequestParam("workerId") int workerId,
-			@RequestParam("month") int month,
-			@RequestParam("year") int year
-			){
-		try {
-			return timeSheetService.report(workerId, month, year);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+//	@RequestMapping(value = "/report")
+//	@ResponseBody
+//	public List<ReportEntryVO> getReportEntries(
+//			@RequestParam("workerId") int workerId,
+//			@RequestParam("month") int month, @RequestParam("year") int year) {
+//		try {
+//			return timeSheetService.report(workerId, month, year);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
+
+	@RequestMapping("/")
+	public String helloWorld(Model model) {
+		model.addAttribute("message", "Hello World!");
+		return "helloWorld";
 	}
-	
-	
+
 }
